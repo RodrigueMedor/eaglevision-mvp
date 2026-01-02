d279911108db1e31f71f66ea7f5b122c96ca1af7
RodrigueMedor <rodriguemedor@yahoo.fr>
2026-01-01 21:06:03 -0500

    graphql endpoint.

--- frontend/src/apollo/client.js
+++ frontend/src/apollo/client.js
@@ -4,8 +4,8 @@ import { onError } from '@apollo/client/link/error';
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
-  // Use environment variable or fallback to production URL
-  uri: process.env.REACT_APP_GRAPHQL_URI || 'https://eaglevisionedge.com/api/graphql',
+  // Point directly to your backend server
+  uri: process.env.REACT_APP_GRAPHQL_URI || 'https://api.eaglevisionedge.com/graphql',
   credentials: 'include',  // Important for sending cookies with CORS
   fetchOptions: {
     mode: 'cors',
