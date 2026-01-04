56c22b82b06e4d02c037ca68acb7e9bf3885a702
RodrigueMedor <rodriguemedor@yahoo.fr>
2026-01-04 18:57:12 -0500

    Fix issues.

--- src/generate_bcrypt_hash.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-# The password to hash
-password = b"testpass"  # Note: must be bytes
-
-# Generate a salt and hash the password
-hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-
-print(f"Hashed password: {hashed.decode('utf-8')}")
--- src/gql/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file makes the graphql directory a Python package
-from .schema import Query, Mutation, AppointmentType, UserType, ContactType
-
-__all__ = ['Query', 'Mutation', 'AppointmentType', 'UserType', 'ContactType']
