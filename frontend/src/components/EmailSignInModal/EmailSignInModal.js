9423bdbb717c59c5c2970c5764bdd62f9163931f
RodrigueMedor <rodriguemedor@yahoo.fr>
2026-01-13 14:31:37 -0500

    Fix issue from server.

--- frontend/src/components/EmailSignInModal/EmailSignInModal.js
+++ frontend/src/components/EmailSignInModal/EmailSignInModal.js
@@ -44,8 +44,8 @@ const LOGIN_MUTATION = gql`
 `;
 
 const SIGNUP_MUTATION = gql`
-  mutation signUp($input: SignUpInput!) {
-    signUp(input: $input) {
+  mutation signup($input: SignUpInput!) {
+    signup(input: $input) {
       success
       message
       user {
